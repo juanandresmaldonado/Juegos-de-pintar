5099ab653baa12eca1f70647b7356eede445f50f
LACG24 <lacg24@utlook.com>
2022-05-13 22:26:14 -0500

    Tercera modificacion

--- Memory.py
+++ Memory.py
@@ -8,8 +8,10 @@ Exercises:
 4. Center single-digit tile.
 5. Use letters instead of tiles.
 """
+import sys
 
 n = 0 #Variable para contar el numero de taps
+f = 0 #Variable que indica cuando el juego termine
 
 from random import *
 from turtle import *
@@ -20,7 +22,6 @@ car = path('car.gif')
 tiles = list(range(8)) * 2 #Ajustamos a 4x4, dando 8 numeros aleatorios
 state = {'mark': None}
 hide = [True] * 16 #Ajustamos a 4x4 dando un maximo de 16
-
 
 def square(x, y):
     """Draw white square with black outline at (x, y)."""
@@ -45,23 +46,30 @@ def xy(count):
 
 
 def tap(x, y):
-    global n
+    global n  #llamada a variable global
+    global f
     """Update mark and hidden tiles based on tap."""
     spot = index(x, y)
     mark = state['mark']
 
     if mark is None or mark == spot or tiles[mark] != tiles[spot]:
         state['mark'] = spot
+
+
     else:
+        f = f + 2 #Se suman dos imagenes encontradas
+
         hide[spot] = False
         hide[mark] = False
         state['mark'] = None
-    
-    n = n + 1 #Actualizar el numero de taps
 
+    n = n + 1 #Actualizar el numero de taps
+    
+    if f == len(hide): #indica cuando todas las imagenes son reveladas
+        print ("Juego terminado")
+        exit()
 
 def draw():
-    global n
     """Draw image and tiles."""
     clear()
     goto(0, 0)
@@ -81,6 +89,8 @@ def draw():
         goto(x + 2, y)
         color('black')
         write(tiles[mark], font=('Arial', 30, 'normal'))
+
+
     write(n, font=('Arial', 10, 'normal'))  #Imprimir el numero de taps
     update()
     ontimer(draw, 100)
